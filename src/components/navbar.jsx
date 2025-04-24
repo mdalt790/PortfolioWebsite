0fb74ebb0995a24d296cb034f65d01744ad788fa
mdalt790 <109238336+mdalt790@users.noreply.github.com>
2025-04-24 00:51:05 -0400

    Clickable Github and Linkedin Icons

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,21 +1,34 @@
 import logo from "../assets/mdIcon2.png"
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
-// <FaInstagram />
-// <FaSquareXTwitter />
-const Navbar = () => {
-  return <nav className="mb-20 flex items-center justify-between py-6">
-    <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-10" src={logo} alt="logo" />
-    </div>
-    <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin href="https://www.linkedin.com/in/michaeldalton790/"/>
-        <FaGithub href="https://github.com/mdalt790"/>
-        
-    </div>
-  </nav>;
-}
 
-export default Navbar
+const Navbar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-10" src={logo} alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a
+          href="https://www.linkedin.com/in/michaeldalton790/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-blue-600"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com/mdalt790"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-gray-800"
+        >
+          <FaGithub />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
